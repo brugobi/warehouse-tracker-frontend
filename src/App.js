d4a07234a858f8b0a3ac9419b296ed35b49a850c
brugobi <brgobbi@hotmail.com>
2021-08-10 16:25:49 -0400

    add signup component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Login from './Components/Login';
+import Signup from './Components/Signup';
 
 function App() {
   return (
     <div className="App">
       <Login />
+      <h1>Signup</h1>
+      <Signup />
     </div>
   );
 }
---
+++ src/Components/Signup.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { createUser } from '../Actions';
+import { useDispatch } from 'react-redux';
+
+function Signup() {
+  const dispatch = useDispatch();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState(0);
+
+  const handleChange = (e) => {
+    let target = e.target.name
+    let val = e.target.value
+    if (target === 'username') {
+      setUsername(val)
+    }
+    if (target === 'password') {
+      setPassword(val)
+    }
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    dispatch(createUser(username, password))
+  }
+
+  return (
+    <div>
+      <label
+      >Username:<input onChange={e => handleChange(e)} type='name' name='username' required />
+      </label>
+      <label
+      >Password:<input onChange={e => handleChange(e)} type='password' name='password' required />
+      </label>
+      <button onClick={e => handleSubmit(e)}>Sign Up</button>
+    </div>
+  );
+}
+
+export default Signup;
