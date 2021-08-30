58acf9d286244c20d921b67b73d33eb98a89ea4f
brugobi <brgobbi@hotmail.com>
2021-08-30 15:07:23 -0400

    add icons to the footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { AiFillHome } from 'react-icons/ai';
+import { BsGraphUp } from 'react-icons/bs';
+import { GrAddCircle } from 'react-icons/gr';
 import { Link } from 'react-router-dom';
 
 function Footer() {
@@ -8,13 +11,13 @@ function Footer() {
         <div className="container">
           <div className="columns is-mobile is-flex">
             <div className="column">
-              <Link to="/CreateItem">Add new Item</Link>
+              <Link to="/CreateItem"><GrAddCircle /></Link>
             </div>
             <div className="column">
-              <Link to="/">Home</Link>
+              <Link to="/"><AiFillHome /></Link>
             </div>
             <div className="column">
-              <Link to="/Statistics">Statistics</Link>
+              <Link to="/Statistics"><BsGraphUp /></Link>
             </div>
           </div>
         </div>
