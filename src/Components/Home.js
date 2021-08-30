5348b2858e5b560330661112750802e124dff13e
brugobi <brgobbi@hotmail.com>
2021-08-30 14:50:37 -0400

    style signup page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,5 @@
-import React from 'react';
 /* eslint-disable */
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Footer from './Footer';
--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
+import { BiLock } from 'react-icons/bi';
+import { FaUser } from 'react-icons/fa';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { createUser } from '../Actions';
 import Nav from './Nav';
+import background from '../img/background.jpg';
 
 function Signup() {
   const history = useHistory();
@@ -31,15 +34,47 @@ function Signup() {
   return (
     <div>
       <Nav />
-      <label htmlFor="name">
-        Username:
-        <input onChange={(e) => handleChange(e)} type="name" name="username" required />
-      </label>
-      <label htmlFor="password">
-        Password:
-        <input onChange={(e) => handleChange(e)} type="password" name="password" required />
-      </label>
-      <button type="button" onClick={(e) => handleSubmit(e)}>Sign Up</button>
+      <div className="login-form-container">
+        <div className="card width-400">
+          <div className="card-image">
+            <figure className="image">
+              <img src={background} alt="placeholder" />
+            </figure>
+          </div>
+          <div className="card-content">
+            <div className="media">
+              <div className="is-size-4">SignUp</div>
+            </div>
+
+            <div className="content">
+              <div className="field">
+                <label htmlFor="name">
+                  <div className="control has-icons-left has-icons-right">
+                    <input onChange={(e) => handleChange(e)} placeholder="username" className="input" type="text" name="username" required />
+                    <span className="icon is-small is-left">
+                      <FaUser />
+                    </span>
+                  </div>
+                </label>
+              </div>
+              <div className="field">
+                <label htmlFor="password">
+                  <div className="control has-icons-left has-icons-right">
+                    <input onChange={(e) => handleChange(e)} placeholder="password" className="input" type="password" name="password" required />
+                    <span className="icon is-small is-left">
+                      <BiLock />
+                    </span>
+                  </div>
+                </label>
+                <div className="field padding-2-5">
+                  <button className="button is-fullwidth is-success" type="button" onClick={(e) => handleSubmit(e)}>Sign Up</button>
+                </div>
+              </div>
+              <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
