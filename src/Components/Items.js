9381a7c166466571c9721fe925a92daa68b30449
brugobi <brgobbi@hotmail.com>
2021-09-01 11:59:11 -0400

    style Items component

--- src/Components/Items.js
+++ src/Components/Items.js
@@ -13,25 +13,33 @@ const Items = () => {
 
   return (
     <>
-      <div className="App">
-        <span>
-          Hey
-          {user.username}
-          , id:
-          {user.id}
-        </span>
-        <ul>
-          {items.length === 0 ? <h2>No items</h2>
-            : items.map((item) => (
-              <li key={item.code}>
-                <p>
-                  {item.name}
-                  {item.id}
-                </p>
-              </li>
-            ))}
-        </ul>
-      </div>
+      {items.length === 0 ? <h2>No items</h2>
+        : items.map((item) => (
+          <section key={item.id}>
+            <div className="container" key={item.code}>
+              <div className="columns items-wrapper">
+                <div className="column courier">
+                  <p className="subtitle color-orange">{item.code}</p>
+                  <p className="is-size-1 title color-blue">{item.name}</p>
+                </div>
+                <div className="column OpenSans-font">
+                  <div className="is-flex is-justify-content-center">
+                    <div className="Comments">
+                      Delete&nbsp;&nbsp;
+                    </div>
+                    <div className="Comments">
+                      |&nbsp;&nbsp;Edit
+                    </div>
+                  </div>
+                </div>
+                <div className="column courier">
+                  <p className="title">Quantity</p>
+                  <p className="is-size-2 subtitle color-blue">{item.current_quantity}</p>
+                </div>
+              </div>
+            </div>
+          </section>
+        ))}
     </>
   );
 };
