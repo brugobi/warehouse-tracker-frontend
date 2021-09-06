322ddebb890753b18ebc3d12761ccb2eb2b2daf1
brugobi <brgobbi@hotmail.com>
2021-09-06 12:34:18 -0400

    add toggle btn to navbar for mobile and style the navbar

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,15 +1,53 @@
-import React from 'react';
+/* eslint-disable */
+import React, { useState } from 'react';
+import { IconContext } from 'react-icons';
+import { FaBoxes } from 'react-icons/fa';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { AiOutlineClose } from 'react-icons/ai';
 import { Link, useHistory } from 'react-router-dom';
 
 function Nav() {
   const username = localStorage.getItem('username');
   const history = useHistory();
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
   return (
-    <nav className="navbar" role="navigation" aria-label="main navigation">
+    <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
         <div className="navbar-item">
-          <div className="brand">WAREHOUSE</div>
+          <div className="navbar-item">
+            <IconContext.Provider value={{ size: '40px', color: '#4c79f2' }}>
+              <FaBoxes />
+            </IconContext.Provider>
+          </div>
+          <div className="navbar-item brand-name is-white">WAREHOUSE</div>
         </div>
+        <div
+          className="navbar-burger navbar-end"
+          onClick={handleClick}
+          onKeyPress={handleClick}
+          role="button"
+          tabIndex="0"
+        >
+          {click ? <AiOutlineClose /> : <GiHamburgerMenu />}
+
+        </div>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          <li
+            onClick={closeMobileMenu}
+            onKeyPress={closeMobileMenu}
+          >
+            <Link to="/Login">Login</Link>
+          </li>
+          <li
+            onClick={closeMobileMenu}
+            onKeyPress={closeMobileMenu}
+          >
+            <Link to="/Signup">Signup</Link>
+          </li>
+        </ul>
       </div>
 
       {username
@@ -17,6 +55,7 @@ function Nav() {
           <div className="navbar-end">
             <div className="navbar-item">
               <button
+                className="button is-link"
                 type="button"
                 onClick={() => {
                   localStorage.removeItem('username');
@@ -28,13 +67,39 @@ function Nav() {
             </div>
           </div>
         ) : (
-          <div className="navbar-end">
-            <div className="navbar-item">
-              <Link to="/Login">Login</Link>
+          <div className="navbar-menu">
+            <div className="navbar-end">
+              <div className="navbar-item">
+                <Link to="/Login">Login</Link>
+              </div>
+              <div className="navbar-item">
+                <Link to="/Signup">Signup</Link>
+              </div>
             </div>
-            <div className="navbar-item">
-              <Link to="/Signup">Signup</Link>
+            {/* <div
+              className="navbar-burger navbar-end"
+              onClick={handleClick}
+              onKeyPress={handleClick}
+              role="button"
+              tabIndex="0"
+            >
+              {click ? <AiOutlineClose /> : <GiHamburgerMenu />}
+
             </div>
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+              <li
+                onClick={closeMobileMenu}
+                onKeyPress={closeMobileMenu}
+              >
+                <Link to="/Login">Login</Link>
+              </li>
+              <li
+                onClick={closeMobileMenu}
+                onKeyPress={closeMobileMenu}
+              >
+                <Link to="/Signup">Signup</Link>
+              </li>
+            </ul> */}
           </div>
         )}
     </nav>
