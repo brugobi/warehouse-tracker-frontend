efd1255ab56c6523a3e866762204c1cbde006d35
brugobi <brgobbi@hotmail.com>
2021-09-10 09:22:22 -0400

    add test for Nav component

---
+++ src/__test__/Nav.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import {
+  BrowserRouter as Router,
+  Link,
+} from 'react-router-dom';
+import renderer from 'react-test-renderer';
+
+test('Link changes the class when clicked', () => {
+  const component = renderer.create(
+    <Router>
+      <Link to="/Login">Login</Link>
+      ,
+      <Link to="/Signup">Signup</Link>
+      ,
+      <Link to="/">LOGOUT</Link>
+      ,
+    </Router>,
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+  // re-rendering
+  tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
--- src/setupTests.js
+++ src/setupTests.js
@@ -8,3 +8,10 @@ import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 Enzyme.configure({ adapter: new Adapter() });
+
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  clear: jest.fn(),
+};
+global.localStorage = localStorageMock;
